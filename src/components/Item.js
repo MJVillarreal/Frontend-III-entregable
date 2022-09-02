a9ff0e8d1884becdc0bb25ddda449a933dc05fd8
MJVillarreal <villarrealmariaj@gmail.com>
2022-09-02 12:10:26 -0300

    sombreado rojo y eliminación de comentarios

--- src/components/Item.js
+++ src/components/Item.js
@@ -18,8 +18,7 @@ export default function Item(props) {
       <h3>{props.nombre}</h3>
       <p>{props.descripcion}</p>
       <h5>
-        {"En stock:"}
-        <span>{cantidad > 0 ? cantidad : "agotado"}</span>
+        {"En stock:"} {cantidad > 0 ? cantidad : <span>{"agotado"}</span>}
       </h5>
       <button disabled={cantidad === 0} onClick={comprarItem}>
         {"Comprar"}
